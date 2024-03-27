547cd66b7c62e4d19072805bbcfe1a1f29fd3592
usama-666 <ejazali8582@gmail.com>
2024-03-27 14:17:43 +0500

    updated api link

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Create a new Axios instance with a base URL
 const API = axios.create({
-  // baseURL: "https://soul-club-niftyzone-final.vercel.app/api/v1", // Your base URL here
-  baseURL: "http://localhost:5000/api/v1", // Your base URL here
+  baseURL: "https://soulclub-server.vercel.app/api/v1", // Your base URL here
+  // baseURL: "http://localhost:5000/api/v1", // Your base URL here
 
   withCredentials: true,
 });
